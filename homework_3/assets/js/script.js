3d0680956a545da4cb984c70d7c58481523d9451
RRam <ranji.ramroop@gmail.com>
2019-10-04 23:02:52 -0500

    changed digit to number in the html, started the JavaScript with definition of the uppercase, lowercase, numbers, and special characters.

---
+++ homework_3/assets/js/script.js
@@ -0,0 +1,12 @@
+
+// my variables here are for the characters used in the password-
+// the upper case, lower case, numbers, and special char.//
+
+var charOptions = [
+    ["uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+    ["lowercase", "abcdefghijklmnopqrstuvwxyz"],
+    ["numbers", "1234567890"],
+    ["symbols", "`~!@#$%^&*()_-+=,.<>?/\|;:"],
+]
+
+document.getElementById()
