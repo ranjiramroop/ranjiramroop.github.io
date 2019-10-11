b21103b60a4faa23280eda15e7c1dbcceeb53571
RRam <ranji.ramroop@gmail.com>
2019-10-11 01:37:40 -0500

    changed the format of the questions to title, choices, answer and adjusting the array for choices through the 'bears' answer.

--- homework_4/assets/js/script.js
+++ homework_4/assets/js/script.js
@@ -7,39 +7,48 @@ var questions = [
         answer: "Cheetah"
     },
     {
-        title: "Which of the following animals has fur?\n(a) Dolphin\n(b) Shark\n(c) Mouse\n(d) Frog",
-        answer: "c"
+        title: "Which of the following animals has fur?",
+        choices: ["Dolphin", "Shark","Mouse", "Frog"],
+        answer: "Mouse"
     },
     {
-        title: "What do fish require to breathe? \n(a) Oxygen\n(b) Helium\n(c) Freon\n(d) Whip-its",
-        answer: "a"
+        title: "What do fish require to breathe?",
+        choices: ["Oxygen", "Helium", "Freon", "Whip-its"],
+        answer: "Oxygen"
     },
     {
-        title: "What animal hibernates in the winter?\n(a) Humans\n(b) Lynx\n(c) Bears\n(d) Walrus",
-        answer: "c"
+        title: "What animal hibernates in the winter?",
+        choices: ["Humans", "Lynx", "Bears", "Walrus"],
+        answer: "Bears"
     },
     {
         title: "Which of these cannot sustain flight?\n(a) Boeing Aircrafts\n(b) Ostrich\n(c) Eagle\n(d) Flamingo",
+        choices: 
         answer: "b"
     },
     {
         title: "A group of lions is known as?\n(a) An Army\n(b) A pack\n(c) A pride\n(d) A squad",
+        choices: 
         answer: "c"
     },
     {
         title: "A female deer is known as?\n(a) A jane\n(b) A doe\n(c) A buck\n(d) An antelope",
+        choices: 
         answer: "b"
     },
     {
         title: "Which of the following animals have the most teeth?\n(a) Elephants\n(b) Armadillos\n(c) Blue Whales\n(d) Snakes",
+        choices: 
         answer: "b"
     },
     {
         title: "Which animal has the longest neck?\n(a) Swan\n(b) Flamingo\n(c) Turtle\n(d) Giraffe",
+        choices: 
         answer: "d"
     },
     {
         title: "Pumba, from Disney's The Lion King movie, is what type of animal?\n(a) Warthog\n(b) Dinner\n(c) Domestic Pig\n(d) Meerkat",
+        choices: 
         answer: "a"
     }
 ];
