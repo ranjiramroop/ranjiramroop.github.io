4c1c6df3fb83d010aa282a0d1f41eb4683b6074e
RRam <ranji.ramroop@gmail.com>
2019-10-11 01:32:43 -0500

    switched the prompt to title in the js file

--- homework_4/assets/js/script.js
+++ homework_4/assets/js/script.js
@@ -7,46 +7,46 @@ var questions = [
         answer: "Cheetah"
     },
     {
-        prompt: "Which of the following animals has fur?\n(a) Dolphin\n(b) Shark\n(c) Mouse\n(d) Frog",
+        title: "Which of the following animals has fur?\n(a) Dolphin\n(b) Shark\n(c) Mouse\n(d) Frog",
         answer: "c"
     },
     {
-        prompt: "What do fish require to breathe? \n(a) Oxygen\n(b) Helium\n(c) Freon\n(d) Whip-its",
+        title: "What do fish require to breathe? \n(a) Oxygen\n(b) Helium\n(c) Freon\n(d) Whip-its",
         answer: "a"
     },
     {
-        prompt: "What animal hibernates in the winter?\n(a) Humans\n(b) Lynx\n(c) Bears\n(d) Walrus",
+        title: "What animal hibernates in the winter?\n(a) Humans\n(b) Lynx\n(c) Bears\n(d) Walrus",
         answer: "c"
     },
     {
-        prompt: "Which of these cannot sustain flight?\n(a) Boeing Aircrafts\n(b) Ostrich\n(c) Eagle\n(d) Flamingo",
+        title: "Which of these cannot sustain flight?\n(a) Boeing Aircrafts\n(b) Ostrich\n(c) Eagle\n(d) Flamingo",
         answer: "b"
     },
     {
-        prompt: "A group of lions is known as?\n(a) An Army\n(b) A pack\n(c) A pride\n(d) A squad",
+        title: "A group of lions is known as?\n(a) An Army\n(b) A pack\n(c) A pride\n(d) A squad",
         answer: "c"
     },
     {
-        prompt: "A female deer is known as?\n(a) A jane\n(b) A doe\n(c) A buck\n(d) An antelope",
+        title: "A female deer is known as?\n(a) A jane\n(b) A doe\n(c) A buck\n(d) An antelope",
         answer: "b"
     },
     {
-        prompt: "Which of the following animals have the most teeth?\n(a) Elephants\n(b) Armadillos\n(c) Blue Whales\n(d) Snakes",
+        title: "Which of the following animals have the most teeth?\n(a) Elephants\n(b) Armadillos\n(c) Blue Whales\n(d) Snakes",
         answer: "b"
     },
     {
-        prompt: "Which animal has the longest neck?\n(a) Swan\n(b) Flamingo\n(c) Turtle\n(d) Giraffe",
+        title: "Which animal has the longest neck?\n(a) Swan\n(b) Flamingo\n(c) Turtle\n(d) Giraffe",
         answer: "d"
     },
     {
-        prompt: "Pumba, from Disney's The Lion King movie, is what type of animal?\n(a) Warthog\n(b) Dinner\n(c) Domestic Pig\n(d) Meerkat",
+        title: "Pumba, from Disney's The Lion King movie, is what type of animal?\n(a) Warthog\n(b) Dinner\n(c) Domestic Pig\n(d) Meerkat",
         answer: "a"
     }
 ];
 var score = 0;
 
 for (var i=0; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt);
+    var response = window.title(questions[i].title);
     if (response == questions[i].answer){
         score++;
         alert("Correct!");
