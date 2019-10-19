f297bfa02db23c9a1da7dbcf1c23a71d683aabba
RRam <ranji.ramroop@gmail.com>
2019-10-19 16:39:50 -0500

    finalized the js file with clear comments

---
+++ homework_5/README.md
@@ -0,0 +1,7 @@
+#  The Business Hour Planner
+## Purpose
+This planner is useful for organizing, and visualizing our tasks that we may have during general business hours of 9am-5pm.
+Store your schedule for that day, and overwrite it if needed! A handy color display allows you to visualize what is approaching for future hours, what is currently scheduled, and what has already past. 
+
+## Usage
+Future hours are designated in a green background color
--- homework_5/assets/js/script.js
+++ homework_5/assets/js/script.js
@@ -2,22 +2,20 @@ var now = moment().format('MMMM Do YYYY, h:mm a');
 // console.log (moment());
 $('#currentDay').html(now);
 
-
-
 //every time user clicks the save button, it should build an array from 9am-5p
 //there should be 9 items in the array
 
 var hour = moment().hour();
 // var id = "#9, #10, #11, #12, #13, #14, #15, #16, #17";
 // console.log(hour); //this gives back the correct hour
-for (var i = 9; i < 18; i++) {
+for (var i = 9; i < 18; i++) { //Start at 9 because my id for 9am starts at 9 and I use that number as the starting point to calculate the later times.
     console.log(i); //makes sure that all the hours are getting called through i
  
     if (i < hour) {
         $("#" + i).addClass("past")  //adding the class here to match the css classes to change color according to time
     }
     else if (i === hour) {
-        $("#" + i).addClass("present")
+        $("#" + i).addClass("present") 
     }
     else {
         $("#" + i).addClass("future")
@@ -26,61 +24,15 @@ for (var i = 9; i < 18; i++) {
 
 $("button").on("click", function(event){
     event.preventDefault();
-    var id = $(this).attr("id")
-    id = id.substring(0,id.length-1);  //when the save button is clicked, the textarea will save the value
-    var text = $("#"+id).val();
-    localStorage.setItem(id, text); // want to save value to local storage
+    var id = $(this).attr("id") //using the id of the button when it is clicked
+    id = id.substring(0,id.length-1);  //converting the button id to the text id by dropping the extra letter on the id. 
+    var text = $("#"+id).val(); //when the save button is clicked, the new variable text will save the value
+    localStorage.setItem(id, text); // saving value to local storage with the key of id and value of text
 })
 
 $(document).ready(function(){
     for (i = 9; i < 18; i++) {
-        var getText = localStorage.getItem(i);
-        $("#"+i).html(getText); // trying to populate the text area with the local storage
+        var getText = localStorage.getItem(i); //getting the stored text from local storage
+        $("#"+i).html(getText); // populate with .html using the text from local storage
     }
 })
-
-
-
-
-
-
-   
-//     // Console log each of the user inputs to confirm we are receiving them
-//     console.log(name);
-//     console.log(email);
-//     console.log(age);
-//     console.log(comment);
-
-//     //first, we retrieve the existing list and turn it into an object
-//     var userList = JSON.parse(localStorage.getItem("users"))
-
-//     //if the list doesn't already exist, create an empty array
-//     if (!userList) {
-//       userList = []
-//     }
-
-//     //push the new user into the array
-//     userList.push({ name, email, age, comment })
-
-//     //update localStorage to the new array 
-//     localStorage.setItem("users", JSON.stringify(userList))
-
-//     //update the display area
-//     updateUserDiv()
-//   });
-
-//   function updateUserDiv() {
-    
-//     //use .pop() to get the last (newest) item in the array
-//     var user = JSON.parse(localStorage.getItem("users")).pop()
-
-//     //update the DOM 
-//     $("#name").text(user.name)
-//     $("#email").text(user.email)
-//     $("#age").text(user.age)
-//     $("#comment").text(user.comment)
-
-//   }
-
-//   //when page first loads, display the most recent user
-//   updateUserDiv()
