5e631735da0d873ac23444e87898cafb27059a82
RRam <ranji.ramroop@gmail.com>
2019-10-19 10:57:59 -0500

    adjusted the hours to be military time in the HTML

--- homework_5/assets/js/script.js
+++ homework_5/assets/js/script.js
@@ -10,7 +10,7 @@ $('#currentDay').html(now);
 var hour = moment().hour();
 var id = "#9, #10, #11, #12, #13, #14, #15, #16, #17";
 //console.log(hour); //this gives back the correct hour
-for (var i = 0; i < 17; i++){
+for (var i = 9; i < 17; i++){
     if (i < hour) {
         $([id] + i).addClass("past")  //adding the class here to match the css classes to change color according to time
     }
@@ -24,15 +24,57 @@ for (var i = 0; i < 17; i++){
 
 $("button").on("click", function(event){
     event.preventDefault();
-    var saveEvent = $("textarea" + saveEvent).val();  //when the save button is clicked, the textarea will save the value
-    localStorage.setItem("scheduled" + event, saveEvent); // value saved to local storage
+    var textEvent = $("textarea" + textEvent).val();  //when the save button is clicked, the textarea will save the value
+    localStorage.setItem("scheduled" + event, textEvent); // value saved to local storage
 })
 
 $(document).ready(function(){
-    for (i = 0; i < 17; i++) {
-        var recallSaveEvent = localStorage.getItem("scheduled" + i);
-        $("textarea" + i).html(recallSaveEvent); // trying to populate the text area with the local storage
+    for (i = 9; i < 17; i++) {
+        var recallTextEvent = localStorage.getItem("scheduled" + i);
+        $("textarea" + i).html(recallTextEvent); // trying to populate the text area with the local storage
     }
     })
 
 
+
+
+   
+//     // Console log each of the user inputs to confirm we are receiving them
+//     console.log(name);
+//     console.log(email);
+//     console.log(age);
+//     console.log(comment);
+
+//     //first, we retrieve the existing list and turn it into an object
+//     var userList = JSON.parse(localStorage.getItem("users"))
+
+//     //if the list doesn't already exist, create an empty array
+//     if (!userList) {
+//       userList = []
+//     }
+
+//     //push the new user into the array
+//     userList.push({ name, email, age, comment })
+
+//     //update localStorage to the new array 
+//     localStorage.setItem("users", JSON.stringify(userList))
+
+//     //update the display area
+//     updateUserDiv()
+//   });
+
+//   function updateUserDiv() {
+    
+//     //use .pop() to get the last (newest) item in the array
+//     var user = JSON.parse(localStorage.getItem("users")).pop()
+
+//     //update the DOM 
+//     $("#name").text(user.name)
+//     $("#email").text(user.email)
+//     $("#age").text(user.age)
+//     $("#comment").text(user.comment)
+
+//   }
+
+//   //when page first loads, display the most recent user
+//   updateUserDiv()
