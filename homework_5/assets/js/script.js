644b40e2b982fe62ac592b51768bee58bfe286ba
RRam <ranji.ramroop@gmail.com>
2019-10-19 05:08:24 -0500

    commenting out the functions, and will come back to them later.

--- homework_5/assets/js/script.js
+++ homework_5/assets/js/script.js
@@ -6,14 +6,16 @@ $('#currentDay').html(now);
 //every time user clicks the save button, it should build an array from 9am-5p
 //there should be 9 items in the array
 
-var scheduleSave = $("textarea");
+//--------------
 
-$("button").on("click", function(saving){
-    saving.preventDefault();
-    localStorage.setItem("scheduled" + i, scheduleSave);
-})
+// var scheduleSave = $("textarea");
 
-$(document).ready(function(){
-        saveButton = localStorage.getItem(i);
-        $(".textarea" + i).html(saveButton);
-    })
+// $("button").on("click", function(saving){
+//     saving.preventDefault();
+//     localStorage.setItem("scheduled" + i, scheduleSave);
+// })
+
+// $(document).ready(function(){
+//         saveButton = localStorage.getItem(i);
+//         $(".textarea" + i).html(saveButton);
+//     })
