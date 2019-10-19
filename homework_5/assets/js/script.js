15b68697e94df54606dd5b82eba4e9ccb7012ec8
RRam <ranji.ramroop@gmail.com>
2019-10-19 06:24:46 -0500

    modified the time color change to be specific to the id of the box. debugging the on click function to figure out why TypeError: (scheduleSave + saving).val is not a function

--- homework_5/assets/js/script.js
+++ homework_5/assets/js/script.js
@@ -8,58 +8,30 @@ $('#currentDay').html(now);
 //there should be 9 items in the array
 
 var hour = moment().hour();
+//console.log(hour); //this does give back the correct hour
 for (var i = 0; i < 9; i++){
     if (i < hour) {
-        $(i).addClass("past")
+        $("#9" + i).addClass("past")  //adding the class here to match the css to change color
     }
     else if (i === hour) {
-        $(i).addClass("present")
+        $("#9"+ i).addClass("present")
     }
     else {
-        $(i).addClass("future")
+        $("#9"+ i).addClass("future")
     }
 }
 
-//--------------
 
-// var scheduleSave = $("textarea");
+var scheduleSave = $("textarea");
 
-// $("button").on("click", function(saving){
-//     saving.preventDefault();
-//     localStorage.setItem("scheduled" + i, scheduleSave);
-// })
+$("button").on("click", function(saving){
+    saving.preventDefault();
+    (scheduleSave + saving).val();
+    localStorage.setItem("scheduled" + saving, scheduleSave);
+})
 
 // $(document).ready(function(){
 //         saveButton = localStorage.getItem(i);
 //         $(".textarea" + i).html(saveButton);
 //     })
 
-
-//--------------------- In CLASS example
-// var randomDate = "02/12/2019";
-//     var randomFormat = "MM/DD/YYYY";
-//     var convertedDate = moment(randomDate, randomFormat);
-
-//     // Using scripts from moment.js write code below to complete each of the following.
-
-//     // Console.log to confirm the code changes you made worked.
-
-//     // 1 Convert the convertedDate into TWO other date formats
-//     console.log(convertedDate.format("MM/DD/YY"));
-//     console.log(convertedDate.format("MMM Do, YYYY hh:mm:ss"));
-//     console.log("----------------------------------------");
-
-//     // 2 ...to determine the time in days between today and the convertedDate
-//     console.log(convertedDate.diff(moment(), "days"));
-//     console.log("----------------------------------------");
-
-//     // 3 ...to determine the number of days between the convertedDate and 02/14/2001
-//     var newDate = moment("04/04/1998", randomFormat);
-//     console.log(convertedDate.diff(newDate, "days"));
-
-//     // 4 ...to convert the convertedDate to unix time (be sure to look up what unix time is)
-//     console.log(convertedDate.format("X"));
-//     console.log("----------------------------------------");
-
-//     // 5 ...to determine what day of the week is convertedDate.
-//     console.log(convertedDate.format("dddd"));
