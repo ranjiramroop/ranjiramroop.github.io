bceccc9f97c788b448ea05d37f94100b2a0d81ca
RRam <ranji.ramroop@gmail.com>
2019-10-19 04:44:33 -0500

    building the javascript. currently getting errors, saving in the meantime and working on debugging

--- homework_5/assets/js/script.js
+++ homework_5/assets/js/script.js
@@ -1,3 +1,17 @@
 var now = moment().format('MMMM Do YYYY, h:mm a');
 // console.log (moment());
 $('#currentDay').html(now);
+
+
+//every time user clicks the save button, it should build an array from 9am-5p
+//there should be 9 items in the array
+
+$("button").onclick.function()
+    event.preventDefault();
+    localStorage.setItem("scheduled");
+
+
+$(document).ready(function(){
+        saveButton = localStorage.getItem(i);
+        $(".textarea" + i).html(saveButton);
+    })
