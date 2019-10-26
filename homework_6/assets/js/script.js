043c3f2eafd0e0853e529cfd054366ef4cf1b045
RRam <ranji.ramroop@gmail.com>
2019-10-26 05:19:13 -0500

    figured out the uv index call for the current weather

--- homework_6/assets/js/script.js
+++ homework_6/assets/js/script.js
@@ -16,7 +16,7 @@ if (localStorage.getItem("cityList")) {
     cityList.push(newCityButton);
     newCity.attr("button");
     localStorage.setItem("cityList", JSON.stringify(cityList));
-    
+    //this is for the current weather
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
     $.ajax({
       url: queryURL,
@@ -26,7 +26,7 @@ if (localStorage.getItem("cityList")) {
         .then(function(response) {
           console.log(queryURL); // Log the queryURL
           console.log(response); // Log the resulting object
-            $(".city").html("<h2>" + response.name + " Current Weather</h1>");
+            $(".city").html("<h2>" + response.name + " Current Weather</h2>");
             $(".wind").text("Wind Speed: " + response.wind.speed);
             $(".humidity").text("Humidity: " + response.main.humidity);
             $(".temp").text("Temperature (F) " + response.main.temp); // Transfer content to HTML
@@ -34,8 +34,21 @@ if (localStorage.getItem("cityList")) {
             console.log("Wind Speed: " + response.wind.speed);
             console.log("Humidity: " + response.main.humidity);
             console.log("Temperature (F): " + response.main.temp);
-          });
-  })
-
+            var lon = response.coord.lon;
+            var lat = response.coord.lat;
+    var uvIndex = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&apikey=" + APIKey;
+    $.ajax({
+      url: uvIndex,
+      method: "GET"
+        })
+        .then(function (response){
+          console.log(uvIndex);
+          console.log(response);
+            $(".uv").text("UV Index: " + response[0].value);
+            console.log("UV Index: " + response[0].value);
+          
+        })
+      });
+    });
       //https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&units=imperial&appid=166a433c57516f51dfab1f7edaed8413
 //line 3 is using the city of bujumbura and will need to be replaced with the json parse info for the city
