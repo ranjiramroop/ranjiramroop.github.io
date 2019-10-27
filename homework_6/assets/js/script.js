889e955707388b1570b539f6488e964a35c9d701
RRam <ranji.ramroop@gmail.com>
2019-10-26 20:43:36 -0500

    adjusted the columns in html and started some css for the newly formed divs from the js

--- homework_6/assets/js/script.js
+++ homework_6/assets/js/script.js
@@ -71,10 +71,10 @@ if (localStorage.getItem("cityList")) {
                 console.log($(".fiveDayForecast"));
                 console.log(forecastWeek);
                 for (var i = 0; i < 5; i++) {
-                  var newDate = $("<div>").addClass("text-black").attr("id", i);
-                  var newIcon = $("<div>").addClass("row" +[i]);
-                  var newTemp = $("<div>").addClass("text-black");
-                  var newHumid = $("<div>").addClass("text-black");
+                  var newDate = $("<div>").addClass("newdate").attr("id", i);
+                  var newIcon = $("<div>").addClass("row new icon" +[i]);
+                  var newTemp = $("<div>").addClass("text-black newtemp");
+                  var newHumid = $("<div>").addClass("text-black newhumid");
                   iconIndex = response.list[i].weather[0].icon;
                   iconURL = "http://openweathermap.org/img/w/" + iconIndex + ".png";
                   iconDiv = $("<img>").addClass("").attr('src', iconURL);
