e9491d4894cdf8ce2c9ab10969ff53c31714c426
RRam <ranji.ramroop@gmail.com>
2019-11-16 19:38:03 -0600

    updating HTML template

--- homework_8/index.js
+++ homework_8/index.js
@@ -2,22 +2,23 @@
 const inquirer = require("inquirer");
 const util = require("util");
 const fs = require("fs");
+const axios = require("axios");
 const writeFileAsync = util.promisify(fs.writeFile);
 
-convertFactory = require('electron-html-to');
-const conversion = convertFactory({
-  converterPath: convertFactory.converters.PDF
-});
-const electron = require('electron');
-conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log(result.numberOfPages);
-  console.log(result.logs);
-  result.stream.pipe(fs.createWriteStream('profile.pdf'));
-  conversion.kill(); // necessary in the electron-server strategy, see below for details
-});
+// convertFactory = require('electron-html-to');
+// const conversion = convertFactory({
+//   converterPath: convertFactory.converters.PDF
+// });
+// const electron = require('electron');
+// conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log(result.numberOfPages);
+//   console.log(result.logs);
+//   result.stream.pipe(fs.createWriteStream('profile.pdf'));
+//   conversion.kill(); // necessary in the electron-server strategy, see below for details
+// });
 
 function promptUser() {
     return inquirer.prompt([
@@ -42,6 +43,24 @@ function promptUser() {
         message: "Where do your reside?"
       }
     ]);
+  }
+
+  async function gitAPICall(answers) {
+    const response = axios
+    .get('https://api.github.com/users/$answers.github}')
+    return response;
+  }
+  async function gitRepoAPICall(answers) {
+    const response = axios
+    .get('https://api.github.com/users/$answers.github}/repos')
+    return response;
+  }
+  function stars(gitRepos) {
+    let startCount=0;
+    for(let i=0; i<gitRepos.data.length; i++){
+      startCount+=gitRepos.data[i].stargazers_count;
+    }
+    return startCount;
   }
 
   promptUser()
